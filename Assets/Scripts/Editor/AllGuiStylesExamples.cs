a9260729b9590b3d906d187bd2db882d2cae31e8
Danila <bezrukov.2002@bk.ru>
2025-04-09 12:37:59 +0300

    add gui styles example

---
+++ Assets/Scripts/Editor/AllGuiStylesExamples.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AllGuiStylesExamples : EditorWindow
+{
+    [MenuItem("Tools/All Gui Styles")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow(typeof(AllGuiStylesExamples));
+        window.titleContent = new GUIContent("All GUI Styles");
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        var width = EditorGUILayout.GetControlRect(GUILayout.Height(0f)).width;
+        foreach (var style in typeof(EditorStyles).GetProperties())
+        {
+            if (style.GetValue(null) is GUIStyle guistyle)
+            {
+                var content = new GUIContent(style.Name);
+                var rect = EditorGUILayout.GetControlRect(false, guistyle.CalcHeight(content, width), guistyle);
+                GUI.Box(rect, content, guistyle);
+            }
+        }
+    }
+}
